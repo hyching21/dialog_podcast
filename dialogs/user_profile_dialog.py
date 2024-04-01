ee98a609ca9f8418f1b9c37f6051d24d40b6107f
hyching21 <124515835+hyching21@users.noreply.github.com>
2024-04-01 16:28:16 +0800

    Update user_profile_dialog.py

--- dialogs/user_profile_dialog.py
+++ dialogs/user_profile_dialog.py
@@ -57,10 +57,10 @@ class UserProfileDialog(ComponentDialog):
         )
 
     async def query_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
-        step_context.values["podcast"] = step_context.result
+        step_context.values["podcast"] = step_context.result.value
 
         await step_context.context.send_activity(
-            MessageFactory.text(f"Your choice is {step_context.values["podcast"]}.")
+            MessageFactory.text(f"Your choice is {step_context.result.value}.")
         )
 
         return await step_context.prompt(
