15f07c9880ad565c13041d5294d787418590d114
hyching21 <124515835+hyching21@users.noreply.github.com>
2024-04-03 14:11:55 +0800

    Add files via upload

--- dialogs/text_processor.py
+++ dialogs/text_processor.py
@@ -1,32 +1,32 @@
 import jieba
 
-class TextProcessor:
-    async def get_transcript(file):
-        with open(file, 'r', encoding='utf-8') as f:
-            text = f.read()
-        return text
+def get_transcript(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+    return text
 
-    async def get_stopword(file):
-        stopword_list = []
-        with open(file, 'r', encoding='utf-8') as f:
-            for line in f:
-                line = line.strip()
-                stopword_list.append(line)
-        return stopword_list
+def get_stopword(file):
+    stopword_list = []
+    with open(file, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            stopword_list.append(line)
+    return stopword_list
 
         
-    async def word_segmentation(text, needRemoveStopwords):
-        seg_list = jieba.lcut(text)
-        filtered_seg_list = []
-        if needRemoveStopwords == True:
-            stopword_list = TextProcessor.get_stopword("stopwords.txt")
-            for word in seg_list:
-                if word not in stopword_list :
-                    if word != ' ':
-                        filtered_seg_list.append(word)
-            return filtered_seg_list
-        else:
-            return seg_list
+def word_segmentation(text, needRemoveStopwords):
+    seg_list = jieba.lcut(text)
+    filtered_seg_list = []
+    if needRemoveStopwords == True:
+        stopword_list = get_stopword("stopwords.txt")
+        for word in seg_list:
+            if word not in stopword_list :
+                if word != ' ':
+                    filtered_seg_list.append(word)
+        return filtered_seg_list
+    else:
+        return seg_list
+    
 
 
 
