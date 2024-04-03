b8476f464ed8c5d6c78ef4b0d6eaaf9f0712ad37
hyching21 <124515835+hyching21@users.noreply.github.com>
2024-04-03 14:48:44 +0800

    testing true

--- dialogs/user_profile_dialog.py
+++ dialogs/user_profile_dialog.py
@@ -80,7 +80,7 @@ class UserProfileDialog(ComponentDialog):
         user_profile.podcast = step_context.values["podcast"]
         user_profile.query = step_context.values["query"]
         processor = TextProcessor()
-        seg_list =  processor.word_segmentation(user_profile.query, False)
+        seg_list =  processor.word_segmentation(user_profile.query, True)
         msg = f"Choice of podcast : {user_profile.podcast} \nYour query : {seg_list}"
         await step_context.context.send_activity(MessageFactory.text(msg))
 
