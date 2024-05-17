0f46e487654ddcc9e4098cbc01bae314f3b1887a
hyching21 <124515835+hyching21@users.noreply.github.com>
2024-05-17 22:39:27 +0800

    Update query_db.py

--- dialogs/query_db.py
+++ dialogs/query_db.py
@@ -72,9 +72,8 @@ class CosmosDBQuery:
     
         cosmos_results = self._batch_query_cosmos_db(list(terms_set))
         # if search result is None
-        if cosmos_results == []:
-            output = None
-            return output
+        if not cosmos_results:
+            return None
         
         doc_ids = {doc['document_id'] for result in cosmos_results for doc in result['documents']}
         docs_details, total, avgdl = self._batch_fetch_document(doc_ids)
